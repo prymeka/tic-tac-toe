e48ca5a3874349c1c49d0bb5d0b84e2eb8d363f0
Aleksander Prymek <prymeka@tcd.ie>
2023-02-11 17:33:14 +0000

    refactoring

--- script.js
+++ script.js
@@ -1,249 +1,309 @@
-const Player = (name, symbol, type) => {
-    let _name = name;
-    let _symbol = symbol;
-    let _type = type;
+const Player = (symbol, type) => {
+  let _symbol = symbol;
+  let _type = type;
 
-    const getName = () => _name;
-    const setName = (newName) => _name = newName;
-    const getSymbol = () => _symbol;
-    const setSymbol = (newSymbol) => _symbol = newSymbol;
-    const getType = () => _type;
-    const setType = (newType) => _type = newType;
+  const getSymbol = () => _symbol;
+  const setSymbol = (newSymbol) => _symbol = newSymbol;
 
-    const _searchInsertIndex = function(array, target) {
-        if (array.indexOf(target) !== -1) {
-            return array.indexOf(target);
-        } else if (target > array[array.length-1]) {
-            return array.length;
-        } else if (target < array[0]) {
-            return 0;
+  const getType = () => _type;
+  const setType = (newType) => _type = newType;
+
+  const easyMode = (computerSelections, playerSelections) => {
+    // select random field
+    let unselected = [];
+    for (let i=0; i<9; i++) {
+      if (!computerSelections.includes(i) && !playerSelections.includes(i)) {
+        unselected.push(i);
+      }
+    }
+    return unselected[Math.floor(Math.random()*unselected.length)];
+  };
+
+  const hardMode = (computerSelections, playerSelections) => {
+    if (computerSelections.length === 0 && playerSelections.length === 0) return 4;
+    // to be implemented
+    let unselected = [];
+    for (let i=0; i<9; i++) {
+      if (!computerSelections.includes(i) && !playerSelections.includes(i)) {
+        unselected.push(i);
+      }
+    }
+    return unselected[Math.floor(Math.random()*unselected.length)];
+  };
+
+  const select = (computerSelections, playerSelections) => {
+    if (_type === "computer-easy") {
+      return easyMode(computerSelections, playerSelections);
+    } else if (_type === "computer-hard") {
+      return hardMode(computerSelections, playerSelections);
+    } else {
+      return;
+    }
+  };
+
+  return {
+    getSymbol, setSymbol,
+    getType, setType,
+    select
+  };
+};
+
+const Gameboard = () => {
+  const _winningSelections = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8]
+  ];
+
+  let _selectionsX = [];
+  let _selectionsO = [];
+
+  const _searchInsertIndex = function(array, target) {
+    if (array.indexOf(target) !== -1) {
+      return array.indexOf(target);
+    } else if (target > array[array.length-1]) {
+      return array.length;
+    } else if (target < array[0]) {
+      return 0;
+    } else {
+      left = 0
+      right = array.length - 1
+      while (left <= right) {
+        mid = Math.floor((left+right)/2)
+        if (array[mid] === target) { 
+          return mid; 
+        } else if (array[mid] < target) {
+          left = mid+1;
         } else {
-            left = 0
-            right = array.length - 1
-            while (left <= right) {
-                mid = Math.floor((left+right)/2)
-                if (array[mid] === target) { 
-                    return mid; 
-                } else if (array[mid] < target) {
-                    left = mid+1;
-                } else {
-                    right = mid-1;
-                }
-            }
-            return left
+          right = mid-1;
         }
-    };
+      }
+      return left;
+    }
+  };
 
-    let _selections = [];
-    const getSelections = () => _selections;
-    const addNewSelection = selection => {
-        const index = _searchInsertIndex(_selections, selection);
-        _selections.splice(index, 0, parseInt(selection));
-    };
-    const resetSelections = () => _selections = [];
-    const removeLastSelection = () => _selections.pop();
+  const getSelections = marker => {
+    if (marker.toLowerCase() === "x") {
+      return _selectionsX;
+    } else if (marker.toLowerCase() === "o") {
+      return _selectionsO;
+    } else {
+      return undefined;
+    }
+  };
+
+  const addNewSelection = (selection, marker) => {
+    if (marker.toLowerCase() === "x") {
+      const index = _searchInsertIndex(_selectionsX, selection);
+      _selectionsX.splice(index, 0, parseInt(selection));
+    } else if (marker.toLowerCase() === "o") {
+      const index = _searchInsertIndex(_selectionsO, selection);
+      _selectionsO.splice(index, 0, parseInt(selection));
+    }
+  };
+
+  const removeLastSelection = (marker) => {
+    if (marker.toLowerCase() === "x") {
+      _selectionsX.pop();
+    } else if (marker.toLowerCase() === "o") {
+      _selectionsO.pop();
+    }
+  };
     
-    return {
-        getName, setName,
-        getSymbol, setSymbol,
-        getType, setType,
-        getSelections, addNewSelection, 
-        resetSelections, removeLastSelection
-    };
+  const checkForWin = marker => {
+    let selectedFields = getSelections(marker);
+    if (selectedFields.length < 3) return false;
+    // since selections are sorted we don't need to check the last two
+    for (let i=0; i<selectedFields.length-2; i++) {
+      let selection = selectedFields[i];
+      // since selections are sorted and we check for the first selection 
+      // in the winning combination, we can skip the 4 and 5
+      if (selection === 4 || selection === 5) continue;
+      for (let j=0; j<_winningSelections.length; j++) {
+        // check if the selection is not equal to the first element of 
+        // any winning combination
+        if (selection !== _winningSelections[j][0]) continue;
+        // since winning combinations are sorted we can skip the rest 
+        // of the for loop if selection less than first element
+        if (selection < _winningSelections[j][0]) break;
+        // if selection is equal and the player won, the other two 
+        // fields in the winning combination must be selected too
+        if (selectedFields.includes(_winningSelections[j][1]) && selectedFields.includes(_winningSelections[j][2])) {
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+
+  const checkForDraw = currentTurn => {
+    if (currentTurn < 9) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const reset = () => {
+    _selectionsX = [];
+    _selectionsO = [];
+  }
+
+  return {
+    getSelections, addNewSelection, removeLastSelection,
+    checkForWin, checkForDraw,
+    reset
+  };
+
 };
 
 const Game = (() => {
-    let currentTurn = 0;
+  // cache DOM
+  const fields = document.querySelectorAll(".field");
+  const undoBtn = document.getElementById("undo-btn");
+  const resetBtn = document.getElementById("reset-btn");
+  const markerBtns = document.querySelectorAll(".marker-btn");
+  const radioBtns = document.querySelectorAll(".radio-level");
+  const modal = document.getElementById("modal");
+  const modalContent = document.getElementById("modal-content");
 
-    // cache DOM
-    const fields = document.querySelectorAll(".field");
+  // initialise 
+  let _currentTurn = 0;
+  let _board = Gameboard();
 
-    const player1Type = document.querySelector(".player-type[name=player-1-type]");
-    const player1Name = document.querySelector(".player-name[name=player-1-name]");
-    const player1Symbol = document.getElementById("player-1-symbol");
-    const player1NameInfo = document.getElementById("player-1-name-info")
-    const player1SymbolInfo = document.getElementById("player-1-symbol-info")
+  let player = Player("X", "player");
+  let computer = Player("O", "computer-easy");
+
+  const playComputer = () => {
+    let marker = computer.getSymbol();
+    let compSelection = computer.select(_board.getSelections(marker), _board.getSelections(player.getSymbol()));
+    _board.addNewSelection(compSelection, marker);
+    // mark selection no the board
+    fields[compSelection].textContent = marker;
+  };
+
+  // marker buttons events
+  markerBtns.forEach(btn => btn.addEventListener("click", e => {
+    // toggle button class
+    markerBtns.forEach(btn => btn.classList.remove("selected"));
+    btn.classList.add("selected");
+    // reset players and the game
+    player = Player(btn.value, "player");
+    computer = Player(btn.value === "X" ? "O" : "X", computer.getType());
+    resetGame();
+    // if computer playes first, make the move
+    if (btn.value === "O") {
+      playComputer();
+      _currentTurn++;
+    }
+  }));
+
+  // radio buttons events
+  radioBtns.forEach(btn => btn.addEventListener("change", e => {
+    computer = Player(computer.getSymbol(), `computer-${e.target.value}`);
+  }));
+
+
+  // fields events
+  const onFieldClick = e => {
+    // ignore populated field
+    if (e.currentTarget.textContent !== "") {
+      return;
+    }
+
+    // player's turn
+    let marker = player.getSymbol();
+    _board.addNewSelection(e.currentTarget.id, marker);
+    // mark selection no the board
+    e.currentTarget.textContent = marker;
+    // check if someone won or game is drawn
+    let isEnd = checkForEnd(marker);
+    _currentTurn++;
     
-    const player2Type = document.querySelector(".player-type[name=player-2-type]");
-    const player2Name = document.querySelector(".player-name[name=player-2-name]");
-    const player2Symbol = document.getElementById("player-2-symbol");
-    const player2NameInfo = document.getElementById("player-2-name-info")
-    const player2SymbolInfo = document.getElementById("player-2-symbol-info")
+    // computer's turn
+    if (isEnd) return;
+    if (_currentTurn < 9) {
+      setTimeout(() => {
+        playComputer();
+        // check if someone won or game is drawn
+        marker = computer.getSymbol();
+        checkForEnd(marker);
+        _currentTurn++;
+      }, 200);
+    }
+  };
 
-    const winnerLabel = document.getElementById("winner-label");
+  fields.forEach(field => field.addEventListener("click", e => onFieldClick(e)));
 
-    const nextBtn = document.getElementById("next-btn");
-    const switchBtn = document.getElementById("switch-btn");
-    const undoBtn = document.getElementById("undo-btn");
-    const resetRoundBtn = document.getElementById("reset-round-btn");
-    const resetGameBtn = document.getElementById("reset-game-btn");
+  // modal events 
+  modal.addEventListener("click", () => {
+    closeModal();
+    resetGame();
+  });
 
-    const score = [document.getElementById("score-1"), document.getElementById("score-2")];
+  const checkForEnd = marker => {
+    let other = marker === "X" ? "O" : "X";
+    if (_board.checkForWin(marker)) {
+      onWin(marker);
+    } else if (_board.checkForWin(other)) {
+      onWin(marker);
+    } else if (_board.checkForDraw(_currentTurn)) {
+      onDraw();
+    } else {
+      return false;
+    }
+    return true;
+  };
 
-    // create players
-    const players = {
-        player1: Player(player1Name.value, player1Symbol.textContent, player1Type.value),
-        player2: Player(player2Name.value, player2Symbol.textContent, player2Type.value),
-        getPlayer: function(string) {
-            string = String(string);
-            if (string.startsWith("player-1") || string === "1") {
-                return this.player1;
-            } else if (string.startsWith("player-2") || string === "2") {
-                return this.player2;
-            } else {
-                return undefined;
-            }
-        },
-        resetSelections: function() {
-            this.player1.resetSelections();
-            this.player2.resetSelections();
-        }
-    };
-    
-    // add events
-    [player1Type, player2Type].forEach(p => p.addEventListener("change", e => {
-        players.getPlayer(e.currentTarget.name).setType(e.currentTarget.value);
-        resetGame();
-    }));
-    [player1Name, player2Name].forEach(p => p.addEventListener("change", e => {
-        const newName = e.currentTarget.value;
-        const playerInfo = p === player1Name ? player1NameInfo : player2NameInfo;
-        playerInfo.textContent = newName;
-        players.getPlayer(e.currentTarget.name).setName(newName);
-    }));
+  const showModal = () => {
+    modal.classList.remove("modal-closed");
+    modal.classList.add("modal-open");
+  };
 
-    switchBtn.addEventListener("click", () => {
-        const oldP1Symbol = player1Symbol.textContent;
-        const oldP2Symbol = player2Symbol.textContent;
+  const closeModal = () => {
+    modal.classList.remove("modal-open");
+    modal.classList.add("modal-closed");
+    modal.addEventListener("animationend", () => {
+      modal.classList.remove("modal-closed");
+    }, {once: true});
+  };
 
-        player1Symbol.textContent = oldP2Symbol;
-        player1SymbolInfo.textContent = oldP2Symbol;
-        players.player1.setSymbol(oldP2Symbol);
+  const onWin = marker => {
+    showModal();
+    modalContent.textContent = `${marker} Has Won. Congratulations!`;
+  };
 
-        player2Symbol.textContent = oldP1Symbol;
-        player2SymbolInfo.textContent = oldP1Symbol;
-        players.player2.setSymbol(oldP1Symbol);
+  const onDraw = () => {
+    showModal();
+    modalContent.textContent = "Draw.";
+  };
 
-        resetGame();
-    });
+  const resetGame = () => {
+    _board.reset();
+    fields.forEach(field => field.textContent = "");
+    _currentTurn = 0;
+  };
 
-    nextBtn.addEventListener("click", () => {
-        nextBtn.style.visibility = "hidden";
-        winnerLabel.textContent = "";
-        players.resetSelections();
-        currentTurn = 0;
-        initBoard();
-    });
+  // buttons events
+  const onUndo = () => {
+    if (_currentTurn < 1) return;
+    _currentTurn = _currentTurn - 2;
+    let seleX = _board.getSelections("X");
+    let lastX = seleX[seleX.length-1];
+    let seleO = _board.getSelections("O");
+    let lastO = seleO[seleO.length-1];
+    fields[lastX].textContent = "";
+    fields[lastO].textContent = "";
+    _board.removeLastSelection("X");
+    _board.removeLastSelection("O");
+  };
 
-    // add events to fields
-    const onFieldClick = e => {
-        if (e.currentTarget.textContent !== "") {
-            return;
-        }
-        // add selection to the player
-        const currentPlayer = players.getPlayer(1+currentTurn%2);
-        currentPlayer.addNewSelection(e.currentTarget.id);
-        // mark selection no the board
-        e.currentTarget.textContent = currentPlayer.getSymbol();
-        // check if someone won
-        if (checkForWin(currentPlayer.getSelections())) {
-            onWin(currentTurn);
-        }
-        currentTurn++;
-    };
+  resetBtn.addEventListener("click", resetGame);
+  undoBtn.addEventListener("click", onUndo);
 
-    const onUndoClick = () => {
-        if (currentTurn < 1) return;
-        currentTurn--;
-        let currentPlayer = players.getPlayer(1+currentTurn%2);
-        let selection = currentPlayer.getSelections();
-        let lastSelection = selection[selection.length-1];
-        let recentField = document.getElementById(String(lastSelection));
-        recentField.textContent = "";
-        recentField.addEventListener("click", onFieldClick, {"once": true})
-        currentPlayer.removeLastSelection();
-    };
-
-    const onResetRoundClick = () => {
-        players.resetSelections();
-        currentTurn = 0;
-        initBoard();
-    };
-
-    const _winningSelections = [
-        [0, 1, 2],
-        [0, 3, 6],
-        [0, 4, 8],
-        [1, 4, 7],
-        [2, 4, 6],
-        [2, 5, 8],
-        [3, 4, 5],
-        [6, 7, 8]
-    ];
-
-    const checkForWin = selectedFields => {
-        if (selectedFields.length < 3) return false;
-        // since selections are sorted we don't need to check the last two
-        for (let i=0; i<selectedFields.length-2; i++) {
-            let selection = selectedFields[i];
-            // since selections are sorted and we check for the first selection 
-            // in the winning combination, we can skip the 4 and 5
-            if (selection === 4 || selection === 5) continue;
-            for (let j=0; j<_winningSelections.length; j++) {
-                // check if the selection is not equal to the first element of 
-                // any winning combination
-                if (selection !== _winningSelections[j][0]) continue;
-                // since winning combinations are sorted we can skip the rest 
-                // of the for loop if selection less than first element
-                if (selection < _winningSelections[j][0]) break;
-                // if selection is equal and the player won, the other two 
-                // fields in the winning combination must be selected too
-                if (selectedFields.includes(_winningSelections[j][1]) && selectedFields.includes(_winningSelections[j][2])) {
-                    _winningSelections[j].forEach(idx => fields[idx].style.color = "green");
-                    nextBtn.style.visibility = "visible";
-                    let winnersName = players.getPlayer(1+currentTurn%2).getName();
-                    if (winnersName === "") {
-                        winnersName = currentTurn%2 === 0 ? "Tom" : "Jerry";
-                    }
-                    winnerLabel.textContent = `${winnersName} has won!`;
-                    return true;
-                }
-            }
-        }
-        return false;
-    };
-    
-    const onWin = () => {
-        // update score
-        let currentPlayersScore = Number(score[currentTurn%2].textContent);
-        score[currentTurn%2].textContent = String(currentPlayersScore+1);
-        // remove event listeners from empty fields
-        fields.forEach(f => f.removeEventListener("click", onFieldClick));
-        // remove event listener from undo and reset buttons
-        undoBtn.removeEventListener("click", onUndoClick);
-        resetGameBtn.removeEventListener("click", resetGame);
-        resetRoundBtn.removeEventListener("click", onResetRoundClick);
-    };
-
-    const initBoard = () => {
-        fields.forEach(f => {
-            f.textContent = "";
-            f.style.color = "white";
-            f.addEventListener("click", onFieldClick, {"once": true});
-        });
-        undoBtn.addEventListener("click", onUndoClick);
-        resetGameBtn.addEventListener("click", resetGame);
-        resetRoundBtn.addEventListener("click", onResetRoundClick);
-    };
-
-    const resetGame = () => {
-        nextBtn.style.visibility = "hidden";
-        winnerLabel.textContent = "";
-        players.resetSelections();
-        currentTurn = 0;
-        score.forEach(s => s.textContent = "0");
-        initBoard();
-    };
-
-    initBoard();
 })();
